2b983b5efdc485f9f1ed4fb6e2bc36b780c075a2
Pietro Celeste <pietro.celeste@gmail.com>
2023-05-27 09:23:31 +0200

    Added test for DOM class

---
+++ test/DOMTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Osynapsy\Html\DOM;
+
+final class DOMTest extends TestCase
+{
+    public function testDOM(): void
+    {
+        DOM::requireCss('base/textbox/style.css');
+        $requiredFiles = DOM::getRequire();        
+        $this->assertIsArray($requiredFiles);
+        $this->assertArrayHasKey('css', $requiredFiles);
+        $this->assertEquals(['css' => ['/assets/vendor/osynapsy/base/textbox/style.css']], $requiredFiles);        
+    }   
+}
