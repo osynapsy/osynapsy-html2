bbcb595fe4b1055077ff8c563e80edfd0d7292d0
Pietro Celeste <p.celeste@osynapsy.net>
2023-07-11 16:23:02 +0200

    Bugfix

--- src/Osynapsy/Html/Component/InputCheckBox.php
+++ src/Osynapsy/Html/Component/InputCheckBox.php
@@ -10,14 +10,15 @@ class InputCheckBox extends Input
 {
     protected $checked;
 
-    public function __construct($id, $class = null, $value = 1)
+    public function __construct($id, $class = null, $value = '1')
     {
+        $this->attribute('value', $value);
         parent::__construct($id, 'checkbox', $class);
-        $this->setValue($value);
     }
 
     public function preBuild()
     {
+
         if ($this->checked) {
             $this->attribute('checked', 'checked');
         }
@@ -34,5 +35,15 @@ class InputCheckBox extends Input
     public function setChecked($condition)
     {
         $this->checked = $condition ? true : false;
+    }
+
+    public function setValue($rawvalue)
+    {
+        $this->value = $rawvalue;
+        if (empty($rawvalue)) {
+            $this->setChecked(false);
+        } elseif ($this->value == $this->getAttribute('value')) {
+            $this->setChecked(true);
+        }
     }
 }
