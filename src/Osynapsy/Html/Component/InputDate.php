008badb8d9a26ffb8b91bfc2ca0be9e3375c4837
Pietro Celeste <p.celeste@osynapsy.net>
2023-08-04 08:43:20 +0200

    Added new component inputcolor, inputdate, inputdatetime, inputTime, InputSearch

---
+++ src/Osynapsy/Html/Component/InputDate.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Osynapsy\Html\Component;
+
+
+class InputDate extends Input
+{
+    public function __construct($id, $class = '')
+    {
+        parent::__construct($id, 'date', $class);
+    }
+
+    public function setMin($minDate)
+    {
+        $this->attribute('min', $minDate);
+    }
+
+    public function setMax($maxDate)
+    {
+        $this->attribute('max', $maxDate);
+    }
+}
---
+++ src/Osynapsy/Html/Component/InputSearch.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Osynapsy package.
+ *
+ * (c) Pietro Celeste <p.celeste@osynapsy.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Osynapsy\Html\Component;
+
+class InputSearch extends Input
+{
+    public function __construct($id, $class = '')
+    {
+        parent::__construct($id, 'search', $class);
+        $this->attribute('spellcheck', 'false');
+    }
+}
